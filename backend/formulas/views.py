3c733a11d6fa9d2b27df11586de353421f62146f
AlEngerre <semen.lyutoev@yandex.ru>
2024-12-08 00:13:55 +0300

    permissions by formulas

--- backend/formulas/views.py
+++ backend/formulas/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework import mixins, viewsets
 from formulas.serializers import *
@@ -11,7 +11,9 @@ from formulas.models import Formula
 class FormulasViewSet(viewsets.ModelViewSet):
     serializer_class = FormulaSerializer
     queryset = Formula.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        queryset = Formula.objects.all().order_by('-creation_date')
+        user = self.request.user
+        queryset = Formula.objects.filter(author=user).order_by('-creation_date')
         return queryset
