9a9b95014596a6f09ff773e47e1ea6f449129d57
Artemitol <artemiy.averin@gmail.com>
2024-12-07 16:44:31 +0300

    добавил лейаут страниц

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -2,11 +2,16 @@ import { Routes, Route } from "react-router-dom"
 import { Homepage } from "@pages/home-page"
 
 import "./main.scss"
+import { DefaultLayout } from "./layouts/default-layou/ui/default-layout"
 
 export function App() {
     return (
-        <Routes>
-            <Route path={"/"} element={<Homepage />} />
-        </Routes>
+        <div className='light text-foreground'>
+            <DefaultLayout>
+                <Routes>
+                    <Route path={"/"} element={<Homepage />} />
+                </Routes>
+            </DefaultLayout>
+        </div>
     )
 }
