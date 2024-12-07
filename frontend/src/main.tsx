a85ac0b05daa242be8d7df63bfc6bfb629428cba
Ashida <raskovalov.valya@gmail.com>
2024-12-07 17:07:22 +0300

    rm persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,16 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { App } from "@app/app"
 import "./main.scss"
-import { PersistGate } from "redux-persist/integration/react"
 import { store } from "./app/store"
 import { Provider } from "react-redux"
-import {persistor} from "./app/store"
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                <BrowserRouter>
+            <BrowserRouter>
                     <App />
-                </BrowserRouter>
-            </PersistGate>
+            </BrowserRouter>
         </Provider>
     </React.StrictMode>
 )
