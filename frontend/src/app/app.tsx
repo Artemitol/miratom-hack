eb5109066fc9c4c4b5ebd894b63a65be9b54b24c
Artemitol <artemiy.averin@gmail.com>
2024-12-07 16:54:52 +0300

    добавил rtk

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -3,15 +3,18 @@ import { Homepage } from "@pages/home-page"
 
 import "./main.scss"
 import { DefaultLayout } from "./layouts/default-layou/ui/default-layout"
+import { Provider } from "react-redux"
 
 export function App() {
     return (
         <div className='light text-foreground'>
-            <DefaultLayout>
-                <Routes>
-                    <Route path={"/"} element={<Homepage />} />
-                </Routes>
-            </DefaultLayout>
+            <Provider store={}>
+                <DefaultLayout>
+                    <Routes>
+                        <Route path={"/"} element={<Homepage />} />
+                    </Routes>
+                </DefaultLayout>
+            </Provider>
         </div>
     )
 }
