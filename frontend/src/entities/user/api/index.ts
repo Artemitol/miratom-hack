54d12887e863b684e13b31697682d735f2ed0e0e
Ashida <raskovalov.valya@gmail.com>
2024-12-07 16:22:53 +0300

    rtk quer setup

---
+++ frontend/src/entities/user/api/index.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import baseApi from '@shared/api/baseApi';
+
+export const userApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getUser: builder.query<{ id: string; name: string }, string>({
+            query: (userId) => `users/${userId}`, 
+        }),
+        updateUser: builder.mutation<{ success: boolean }, { id: string; name: string }>({
+            query: (body) => ({
+                url: `users/${body.id}`,
+                method: 'PUT',
+                body,
+            }),
+        }),
+    }),
+});
+
+export const { useGetUserQuery, useUpdateUserMutation } = userApi; 
