fffbd6816134288a91bf69c8d73f63bf38ff0070
Artemitol <artemiy.averin@gmail.com>
2024-12-07 16:56:28 +0300

    добавил в провайдер стор

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -4,11 +4,12 @@ import { Homepage } from "@pages/home-page"
 import "./main.scss"
 import { DefaultLayout } from "./layouts/default-layou/ui/default-layout"
 import { Provider } from "react-redux"
+import { store } from "./store/store"
 
 export function App() {
     return (
         <div className='light text-foreground'>
-            <Provider store={}>
+            <Provider store={store}>
                 <DefaultLayout>
                     <Routes>
                         <Route path={"/"} element={<Homepage />} />
